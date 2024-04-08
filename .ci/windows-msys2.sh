887ce1deea215ea38540c7d14a6748666eab353c
Reg Tiangha <rtiangha@users.noreply.github.com>
2024-04-08 12:55:52 -0600

    workflows: fix windows build

--- .ci/windows-msys2.sh
+++ .ci/windows-msys2.sh
@@ -1,12 +1,7 @@
 #!/bin/sh -ex
 
-if [ "$TARGET" = "msvc" ]; then
-    export EXTRA_CMAKE_FLAGS=(-DCMAKE_CXX_FLAGS="/O2" -DCMAKE_C_FLAGS="/O2")
-else
-    export EXTRA_CMAKE_FLAGS=(-DCMAKE_CXX_FLAGS="-O2" -DCMAKE_C_FLAGS="-O2")
-fi
-
 mkdir build && cd build
+
 cmake .. -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_C_COMPILER_LAUNCHER=ccache \
@@ -14,7 +9,10 @@ cmake .. -G Ninja \
     -DENABLE_QT_TRANSLATION=ON \
     -DCITRA_ENABLE_COMPATIBILITY_REPORTING=ON \
     -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON \
+    -DCMAKE_CXX_FLAGS="-O2" \
+    -DCMAKE_C_FLAGS="-O2" \
     -DUSE_DISCORD_PRESENCE=ON
+
 ninja
 ninja bundle
 strip -s bundle/*.exe
